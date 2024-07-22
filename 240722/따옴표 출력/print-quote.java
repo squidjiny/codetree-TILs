dbec41cabdf307eb0bb8178f410b13861d891fe9
김정아 <92552047+squidjiny@users.noreply.github.com>
2024-07-22 20:04:16 +0900

    Title: 따옴표 출력, Time: 83ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240722/따옴표 출력/print-quote.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        System.out.println("He says \"It's a really simple sentence\".");
+    }
+}

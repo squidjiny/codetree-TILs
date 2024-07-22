711dee400b2617224031581922e730e81d438b6b
김정아 <92552047+squidjiny@users.noreply.github.com>
2024-07-22 20:14:19 +0900

    Title: 정수 선언하고 뺄셈, Time: 84ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240722/정수 선언하고 뺄셈/define-numbers-and-substract.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        int a, b;
+        a = 97;
+        b = 13;
+        int c = a-b;
+        System.out.println(a + " - " + b + " = " + c);
+    }
+}
